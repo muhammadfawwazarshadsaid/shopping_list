7933bb2d2df12b622b0111ec58d624d2f1d7d5a8
muhammadfawwazarshads <muhammad.fawwaz22@ui.ac.id>
2023-09-20 13:32:25 +0700

    Fdd

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from main.views import show_json, show_main, create_product, show_xml 
+from main.views import register #sesuaikan dengan nama fungsi yang dibuat
 
 app_name = 'main'
 
@@ -7,5 +8,9 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-product', create_product, name='create_product'),
     path('json/', show_json, name='show_json'), 
+    path('register/', register, name='register'), 
+
+
+
     
 ]
--- main/views.py
+++ main/views.py
@@ -4,6 +4,10 @@ from main.forms import ProductForm
 from django.urls import reverse
 from django.http import HttpResponse
 from django.core import serializers
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages  
+from django.contrib.auth import authenticate, login
 
 from main.models import Product
 
@@ -44,3 +48,27 @@ def show_json_by_id(request, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main:show_main')
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
